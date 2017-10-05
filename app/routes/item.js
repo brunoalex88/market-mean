5a8f636e2c732c85e57cefb159181cd25b87d530
Bruno Oliveira <brunoalex_88@yahoo.com.br>
2017-10-05 15:35:24 -0300

    Criação de rotas para o item

---
+++ app/routes/item.js
@@ -0,0 +1,13 @@
+module.exports = function(app) {
+    
+    var controller = app.controllers.item;
+
+    app.route('/item')
+        .get(controller.listItems)
+        .post(controller.salvaItem);
+
+    app.route('/item/:id')
+        .get(controller.listItem)
+        .delete(controller.removeItem);
+
+};
