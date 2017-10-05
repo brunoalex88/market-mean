3a2171fb3d66642d70251e2b94b42056addc2ebd
Bruno Oliveira <brunoalex_88@yahoo.com.br>
2017-10-05 14:52:35 -0300

    Adicionado express

--- server.js
+++ server.js
@@ -1,1 +1,6 @@
 var http = require('http');
+var app = require('./config/express')();
+
+http.createServer(app).listen(app.get('port'), function() {
+    console.log('Express listening on: localhost:' + app.get('port'));
+});
