cd815852ba4e8ab5c4ce8c847a343f34cf9c86dc
Bruno Oliveira <brunoalex_88@yahoo.com.br>
2017-10-07 19:04:46 -0300

    Inclusão de modal para atualizar a descrição e quantidades de estoque do item.

--- public/js/controllers/ItemController.js
+++ public/js/controllers/ItemController.js
@@ -2,6 +2,19 @@ angular.module('market-mean').controller('ItemController', function($scope, Item
 
     $scope.produtos = [];
     $scope.mensagem = {texto: ''};
+    $scope.filtro = '';
+    $scope.produto = {};
+
+    $scope.alterar = function(produto) {
+        $scope.produto = produto;
+    };
+
+    $scope.salvarProduto = function() {
+        Item.save($scope.produto, function() {
+            mensagem = {'texto': 'Produto salvo!'};
+            listaProdutos();
+        });
+    };
 
     function listaProdutos() {
         Item.query(function(produtos) {
