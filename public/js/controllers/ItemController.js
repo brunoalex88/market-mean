94794dfd2ead3424f64a366c5531f12e7785c359
Bruno Oliveira <brunoalex_88@yahoo.com.br>
2017-10-09 17:26:48 -0300

    Adaptações do modal para incluir/alterar o produto

--- public/js/controllers/ItemController.js
+++ public/js/controllers/ItemController.js
@@ -4,9 +4,19 @@ angular.module('market-mean').controller('ItemController', function($scope, Item
     $scope.mensagem = {texto: ''};
     $scope.filtro = '';
     $scope.produto = {};
+    $scope.modalTitle = '';
 
     $scope.alterar = function(produto) {
-        $scope.produto = produto;
+        if (typeof produto === "undefined") {
+            $scope.produto = {
+                descricao: "",
+                estoqueMinimo: 0,
+                estoqueAtual: 0};
+            $scope.modalTitle = 'NOVO PRODUTO'
+        } else {
+            $scope.modalTitle = 'ALTERAR PRODUTO';
+            $scope.produto = produto;
+        }
     };
 
     $scope.salvarProduto = function() {
@@ -16,6 +26,38 @@ angular.module('market-mean').controller('ItemController', function($scope, Item
         });
     };
 
+    $scope.aumentaEstoqueMinimo = function() {
+        $scope.produto.estoqueMinimo++;
+    };
+
+    $scope.diminuiEstoqueMinimo = function() {
+        $scope.produto.estoqueMinimo--;
+    };
+
+    $scope.aumentaEstoqueAtual = function() {
+        $scope.produto.estoqueAtual++;
+    };
+
+    $scope.diminuiEstoqueAtual = function() {
+        $scope.produto.estoqueAtual--;
+    };
+
+    $scope.listaProdutos = function() {
+        listaProdutos();
+    };
+
+    $scope.remover = function(produto) {
+        Item.delete({id: produto._id},
+            listaProdutos,
+            function(erro) {
+                console.log('Erro ao excluir o item: ' + erro);
+                $scope.mensagem = {
+                    texto: 'Erro ao excluir o item: ' + erro
+                };
+            }
+        );
+    };
+
     function listaProdutos() {
         Item.query(function(produtos) {
             $scope.produtos = produtos;
@@ -23,7 +65,7 @@ angular.module('market-mean').controller('ItemController', function($scope, Item
         },
         function(erro) {
             console.log('Erro ao buscar os itens cadastrados: ' + erro);
-            $scope.mensagem = {texto: 'Erro ao buscar os itens cadastrados: ' + erro};
+            $scope.mensagem = {texto: 'Erro ao buscar os itens cadastrados: ' + erro.message};
         });
     };
 
--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,7 @@
 angular.module('market-mean', ['ngRoute', 'ngResource', 'angular-loading-bar'])
-     .config(function($routeProvider) {   
+     .config(function($routeProvider, $locationProvider) {   
+
+        $locationProvider.hashPrefix('');
 
 		$routeProvider.when('/item', {
 			templateUrl: 'partials/item.html',
@@ -8,4 +10,21 @@ angular.module('market-mean', ['ngRoute', 'ngResource', 'angular-loading-bar'])
         
         $routeProvider.otherwise({redirectTo: '/'});
 
+    })
+     .directive('onlyDigits', function () {   
+        return {
+            restrict: 'A',
+            require: '?ngModel',
+            link: function (scope, element, attrs, modelCtrl) {
+                modelCtrl.$parsers.push(function (inputValue) {
+                    if (inputValue == undefined) return '';
+                    var transformedInput = inputValue.replace(/[^0-9]/g, '');
+                    if (transformedInput !== inputValue) {
+                        modelCtrl.$setViewValue(transformedInput);
+                        modelCtrl.$render();
+                    }
+                    return transformedInput;
+                });
+            }
+        };
     });
