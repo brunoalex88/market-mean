b9e2e1256bbc3a8605bbcb8c0be68a65f469f1fb
Oliveira <bruno.oliveira3@hpe.com>
2017-10-13 15:42:07 -0300

    Alteração para atribuir porta dinamica

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ var app = require('./config/express')();
 //require('./config/database')('mongodb://localhost/market-mean');
 require('./config/database')('mongodb://heroku_s9bqcv5m:4j2nuumpo8kelfuirseudmsf9m@ds045608.mlab.com:45608/heroku_s9bqcv5m');
 
-http.createServer(app).listen(app.get('port'), function() {
+http.createServer(app).listen(process.env.PORT || app.get('port'), function() {
     console.log('Express listening on: localhost:' + app.get('port'));
 });
