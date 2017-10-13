3749b8e62879dfc47b4f2b7962fce37584778786
Oliveira <bruno.oliveira3@hpe.com>
2017-10-13 17:03:55 -0300

    .

--- app/models/Item.js
+++ app/models/Item.js
@@ -17,8 +17,6 @@ module.exports = function() {
         }
     });
 
-    var model = mongoose.model('Item', schema);
-    console.log('Model: ' + model);
-    return model;
+    return mongoose.model('Item', schema);
 
 };
