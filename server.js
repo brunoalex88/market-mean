f10220b272639578114c79ebb0d606a783c178e2
Oliveira <bruno.oliveira3@hpe.com>
2017-10-13 16:29:23 -0300

    Apontando para o mongodb do Heroku

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 var http = require('http');
 var app = require('./config/express')();
-require('./config/database')('mongodb://localhost/market-mean');
 var DepLinker = require('dep-linker');
-DepLinker.copyDependenciesTo('./public/vendor')
+require('./config/database')('mongodb://localhost/market-mean');
 //require('./config/database')('mongodb://heroku_s9bqcv5m:4j2nuumpo8kelfuirseudmsf9m@ds045608.mlab.com:45608/heroku_s9bqcv5m');
+
+DepLinker.copyDependenciesTo('./public/vendor')
 
 http.createServer(app).listen(process.env.PORT || app.get('port'), function() {
     console.log('Express listening on: localhost:' + app.get('port'));
