320d78932e8ff7ada719c8f629ddce7a93001233
Oliveira <bruno.oliveira3@hpe.com>
2017-10-13 15:36:04 -0300

    Alteração do endereço do banco para instancia do mongodb do Heroku

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var http = require('http');
 var app = require('./config/express')();
-require('./config/database')('mongodb://localhost/market-mean');
+//require('./config/database')('mongodb://localhost/market-mean');
+require('./config/database')('mongodb://heroku_s9bqcv5m:4j2nuumpo8kelfuirseudmsf9m@ds045608.mlab.com:45608/heroku_s9bqcv5m');
 
 http.createServer(app).listen(app.get('port'), function() {
     console.log('Express listening on: localhost:' + app.get('port'));
