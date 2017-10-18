726c7064400f5e7048a0b5ee3e7a1a92b25afaf7
Oliveira <bruno.oliveira3@hpe.com>
2017-10-18 15:36:38 -0200

    Remoção de comentários

--- server.js
+++ server.js
@@ -1,11 +1,8 @@
 var http = require('http');
 var app = require('./config/express')();
-//var DepLinker = require('dep-linker');
 var uriMongo = process.env.MONGOLAB_URI || 'mongodb://localhost/market-mean';
 //require('./config/database')('mongodb://localhost/market-mean');
 require('./config/database')('mongodb://heroku_s9bqcv5m:4j2nuumpo8kelfuirseudmsf9m@ds045608.mlab.com:45608/heroku_s9bqcv5m');
-
-//DepLinker.copyDependenciesTo('./public/vendor')
 
 http.createServer(app).listen(process.env.PORT || app.get('port'), function() {
     console.log('Express listening on: localhost:' + app.get('port'));
