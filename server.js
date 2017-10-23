0ef182f5f07240b80459e3fbcfdc3df3e2a330b5
Oliveira <bruno.oliveira3@hpe.com>
2017-10-23 16:08:21 -0200

    Alterações de link do MongoDB

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
+//mongodb://heroku_s9bqcv5m:4j2nuumpo8kelfuirseudmsf9m@ds045608.mlab.com:45608/heroku_s9bqcv5m
 var http = require('http');
 var app = require('./config/express')();
 var uriMongo = process.env.MONGOLAB_URI || 'mongodb://localhost/market-mean';
 //require('./config/database')('mongodb://localhost/market-mean');
 require('./config/database')(uriMongo);
+require('./config/passport')();
+
+console.log('URI Mongo: ' + uriMongo);
 
 http.createServer(app).listen(process.env.PORT || app.get('port'), function() {
-    console.log('Express listening on: localhost:' + app.get('port'));
+    console.log('Express server listening');
 });
